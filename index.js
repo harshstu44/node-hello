adf586603a9f386d483cee7710dee3a67ba90c73
Harsh Choudhary <harsh221006ec@sdbc.ac.in>
2026-07-04 15:22:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const server = http.createServer((req, res) => {
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
+// Add this snippet inside index.js to print out custom env vars
+app.get('/test', (req, res) => {
+  res.json({
+    message: "Checking environment variables",
+    PORT_RECEIVED: process.env.PORT,
+    MY_TEST_SECRET: process.env.MY_TEST_SECRET || "Not Set",
+    NODE_ENV: process.env.NODE_ENV || "development"
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
